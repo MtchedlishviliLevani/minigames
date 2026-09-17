5861cb99e8ed4eb38e570e747d1cfad92bac1f2a
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-09-18 01:27:12 +0400

    feat: add footer links data and social icons

--- src/components/Icon/Icon.ts
+++ src/components/Icon/Icon.ts
@@ -1,12 +1,19 @@
 import './Icon.scss';
 import arrowBack from '../../assets/icons/arrow-back.svg?raw';
 import arrowForward from '../../assets/icons/arrow-forward.svg?raw';
+import chat from '../../assets/icons/chat.svg?raw';
 import close from '../../assets/icons/close.svg?raw';
+import code from '../../assets/icons/code.svg?raw';
 import heart from '../../assets/icons/heart.svg?raw';
+import lock from '../../assets/icons/lock.svg?raw';
+import mail from '../../assets/icons/mail.svg?raw';
 import menu from '../../assets/icons/menu.svg?raw';
 import puzzle from '../../assets/icons/puzzle.svg?raw';
+import rss from '../../assets/icons/rss.svg?raw';
+import share from '../../assets/icons/share.svg?raw';
 import star from '../../assets/icons/star.svg?raw';
 import upload from '../../assets/icons/upload.svg?raw';
+import user from '../../assets/icons/user.svg?raw';
 
 const ICONS = {
   menu,
@@ -15,6 +22,13 @@ const ICONS = {
   star,
   heart,
   upload,
+  share,
+  chat,
+  rss,
+  code,
+  mail,
+  lock,
+  user,
   'arrow-back': arrowBack,
   'arrow-forward': arrowForward,
 };
