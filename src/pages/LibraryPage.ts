16af8a01a9e2424f52085e56bce49ce5ca4c74a0
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-09-26 01:05:13 +0400

    feat: add the library pagination section

--- src/pages/LibraryPage.ts
+++ src/pages/LibraryPage.ts
@@ -1,5 +1,11 @@
 import { LibraryFilters } from '../components/LibraryFilters/LibraryFilters';
 import { LibraryGames } from '../components/LibraryGames/LibraryGames';
 import { LibraryIntro } from '../components/LibraryIntro/LibraryIntro';
+import { LibraryPagination } from '../components/LibraryPagination/LibraryPagination';
 
-export const LibraryPage = (): Node[] => [LibraryIntro(), LibraryFilters(), LibraryGames()];
+export const LibraryPage = (): Node[] => [
+  LibraryIntro(),
+  LibraryFilters(),
+  LibraryGames(),
+  LibraryPagination(),
+];
